8493b0ca134817018183c9738bcbc9e71fa89ac6
vahap iren <vahapiren@gmail.com>
2022-12-31 16:46:10 +0300

    day45: Abstraction

---
+++ src/day45_AbstractionContinue_Interface/Extends_Vs_Interfaces.java
@@ -0,0 +1,36 @@
+package day45_AbstractionContinue_Interface;
+
+        //extends
+
+    class C{
+
+    }
+    class A{
+
+    }
+    class B extends A{
+
+    }
+    interface Z{
+
+    }
+    interface X{
+
+    }
+    interface Y extends X,Z{
+
+    }
+
+    //interfaces
+
+    class D implements X,Y,Z{
+
+    }
+
+
+
+
+public class Extends_Vs_Interfaces extends A implements X,Y,Z{
+
+
+}
